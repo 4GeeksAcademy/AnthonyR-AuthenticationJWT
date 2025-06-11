387919cbf930d820ebbb93a30a0804850498da8b
Tonyreng <tonyreng01@gmail.com>
2025-06-11 03:20:45 +0000

    Configuración de ambiente

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+
 
 api = Blueprint('api', __name__)
 
